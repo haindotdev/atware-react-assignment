d0d642605acade93128b1f461e4612b64e0534fd
kienvo <kien.votrung@atware.asia>
2026-03-12 18:22:16 +0700

    ft: implement choose meal form

--- src/core/features/OrderFood/OrderFoodView.tsx
+++ src/core/features/OrderFood/OrderFoodView.tsx
@@ -1,4 +1,5 @@
 import ChooseDishForm from './components/ChooseDishForm';
+import ChooseMealForm from './components/ChooseMealForm.tsx';
 import ChooseRestaurantForm from './components/ChooseRestaurantForm';
 import OrderFoodProvider, { useOrderFood } from './components/OrderFoodProvider';
 import dishData from '../../../../data/dishes.json';
@@ -15,7 +16,7 @@ export default function OrderFoodView({}: Readonly<Props>) {
     <OrderFoodProvider>
       <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
         <div className="w-full md:w-1/2 bg-white border-2 border-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] rounded-xl p-10 flex flex-col items-center gap-10">
-          {step === 1 && <div>Choose Meal Form</div>}
+          {step === 1 && <ChooseMealForm />}
           {step === 2 && <ChooseRestaurantForm />}
           {step === 3 && <ChooseDishForm />}
           {step === 4 && <div>Choose Meal Form</div>}
